66d0606f9f00fded76d40e05e58aa55d6b0e561e
Tiffany Lee <tl5110@g.rit.edu>
2023-04-14 17:07:05 -0400

    Implemented and tested FIFOTSQueue and PriorityTSQueue

--- src/edu/rit/cs/grocerystore/FIFOTSQueue.java
+++ src/edu/rit/cs/grocerystore/FIFOTSQueue.java
@@ -3,22 +3,40 @@ package edu.rit.cs.grocerystore;
 import java.util.LinkedList;
 import java.util.List;
 
-// TODO
-// This code is provided so that the initial code compiles.
-// It is up to you to fill it in properly and add comments.
+/**
+ * An implementation of TSQueue that follows first-in-first-out behavior
+ *
+ * @param <E> The type of elements that will be in the queue
+ * @author Tiffany Lee
+ */
 
 public class FIFOTSQueue< E extends TimedObject > implements TSQueue< E > {
+    private final LinkedList<E> contents;
 
     public FIFOTSQueue() {
+        this.contents = new LinkedList<>();
     }
 
     @Override
     public synchronized int enqueue( E value ) {
-        return 0;
+        contents.add(value);
+        value.enterQueue();
+        notifyAll();
+        return contents.size();
     }
 
     @Override
     public synchronized E dequeue() {
-        return null;
+        while(contents.isEmpty()){
+            try{
+                wait();
+            } catch(InterruptedException e){
+                throw new RuntimeException(e);
+            }
+        }
+        E removed = contents.remove();
+        removed.exitQueue();
+        notifyAll();
+        return removed;
     }
 }
--- src/edu/rit/cs/grocerystore/PriorityTSQueue.java
+++ src/edu/rit/cs/grocerystore/PriorityTSQueue.java
@@ -2,24 +2,42 @@ package edu.rit.cs.grocerystore;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 
-// TODO
-// This code is provided so that the initial code compiles.
-// It is up to you to fill it in properly and add comments.
+/**
+ * A TSQueue with priority queue extraction order.
+ *
+ * @param <E> the type of elements that will be in the queue (must be Comparable)
+ * @author Tiffany Lee
+ */
 
 public class PriorityTSQueue< E extends TimedObject & Comparable< E > >
         implements TSQueue< E > {
-
+    private final PriorityQueue<E> contents;
     public PriorityTSQueue() {
+        this.contents = new PriorityQueue<>();
     }
 
     @Override
     public synchronized int enqueue( E value ) {
-        return 0;
+        contents.add(value);
+        value.enterQueue();
+        notifyAll();
+        return contents.size();
     }
 
     @Override
     public synchronized E dequeue() {
-        return null;
+        while(contents.isEmpty()){
+            try{
+                wait();
+            } catch(InterruptedException e){
+                throw new RuntimeException(e);
+            }
+        }
+        E removed = contents.remove();
+        removed.exitQueue();
+        notifyAll();
+        return removed;
     }
 }
