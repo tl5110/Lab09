eb7c2cf4adc893981543f6ce3365d880350f8d4c
Tiffany Lee <tl5110@g.rit.edu>
2023-04-17 00:25:57 -0400

    Added docstring to FIFOTSQueue and PriorityTSQueue classes

--- src/edu/rit/cs/grocerystore/FIFOTSQueue.java
+++ src/edu/rit/cs/grocerystore/FIFOTSQueue.java
@@ -10,13 +10,23 @@ import java.util.List;
  * @author Tiffany Lee
  */
 
-public class FIFOTSQueue< E extends TimedObject > implements TSQueue< E > {
+public class FIFOTSQueue<E extends TimedObject> implements TSQueue<E> {
+    /** The queue, or checkout line */
     private final LinkedList<E> contents;
 
+    /** Initialize the underlying data structure used for the queue */
     public FIFOTSQueue() {
         this.contents = new LinkedList<>();
     }
 
+    /**
+     * Puts the value in the queue, and calls TimedObject.enterQueue() on
+     * the value. This method is synchronized because its body is a
+     * critical region.
+     *
+     * @param value the value to be enqueued
+     * @return the size of the queue after the value was added
+     */
     @Override
     public synchronized int enqueue( E value ) {
         contents.add(value);
@@ -25,6 +35,14 @@ public class FIFOTSQueue< E extends TimedObject > implements TSQueue< E > {
         return contents.size();
     }
 
+    /**
+     * Removes a value from the queue and calls TimedObject.exitQueue() on
+     * the value. This method is expected to block(wait) if the queue is empty,
+     * rather than throwing an exception or returning a null value. This method
+     * is synchronized because its body is a critical region.
+     *
+     * @return the value that has been in the queue for the longest period of time
+     */
     @Override
     public synchronized E dequeue() {
         while(contents.isEmpty()){
--- src/edu/rit/cs/grocerystore/PriorityTSQueue.java
+++ src/edu/rit/cs/grocerystore/PriorityTSQueue.java
@@ -11,13 +11,24 @@ import java.util.PriorityQueue;
  * @author Tiffany Lee
  */
 
-public class PriorityTSQueue< E extends TimedObject & Comparable< E > >
+public class PriorityTSQueue<E extends TimedObject & Comparable<E>>
         implements TSQueue< E > {
+    /** The queue, or checkout line */
     private final PriorityQueue<E> contents;
+
+    /** Initialize the underlying data structure used for the queue */
     public PriorityTSQueue() {
         this.contents = new PriorityQueue<>();
     }
 
+    /**
+     * Puts the value in the queue, and calls TimedObject.enterQueue() on
+     * the value. This method is synchronized because its body is a
+     * critical region
+     *
+     * @param value the value to be enqueued
+     * @return the size of the queue after the value was added
+     */
     @Override
     public synchronized int enqueue( E value ) {
         contents.add(value);
@@ -26,6 +37,14 @@ public class PriorityTSQueue< E extends TimedObject & Comparable< E > >
         return contents.size();
     }
 
+    /**
+     * Removes a value from the queue and calls TimedObject.exitQueue()
+     * on the value. This method is expected to block(wait) if the queue is
+     * empty, rather than throwing an exception or returning a null value.
+     * This method is synchronized because its body is a critical region.
+     *
+     * @return the minimum value in the queue, according to E's natural ordering
+     */
     @Override
     public synchronized E dequeue() {
         while(contents.isEmpty()){
